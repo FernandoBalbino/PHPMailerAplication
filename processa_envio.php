0a0b750cc013613b40d1f4cb1c892ed2a7717978
Fernando Kauã <80929113+FernandoBalbino@users.noreply.github.com>
2021-06-24 12:02:24 -0300

    Update processa_envio.php

--- processa_envio.php
+++ processa_envio.php
@@ -55,14 +55,14 @@
 			$mail->isSMTP();                                            //Send using SMTP
 			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-			$mail->Username   = 'fernandokauadasilvabalbino@gmail.com
+			$mail->Username   = 'seuemail@gmail.com
 ';                     //SMTP username
-			$mail->Password   = 'vinho123';                               //SMTP password
+			$mail->Password   = 'suasenha123';                               //SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
 			//Recipients
-			$mail->setFrom('fernandokauadasilvabalbino@gmail.com
+			$mail->setFrom('seuemail@gmail.com
 ', 'Cliente site contato');
 			$mail->addAddress($mensagem->__get('para'));     //Add a recipient
 			//$mail->addReplyTo('info@example.com', 'Information');
